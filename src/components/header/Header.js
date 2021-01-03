3fef13c811e701d7a71cfdd88743ee17de1479bb
azatmagdeev <azatmagdeev@gmail.com>
2021-01-03 21:23:35 +0300

    framework-start merged to master

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,8 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 
-// eslint-disable-next-line require-jsdoc
 export class Header extends ExcelComponent {
   static className = 'excel__header'
-  // eslint-disable-next-line require-jsdoc
   toHTML() {
-    // super.toHTML();
     return `<label>
             <input type="text" class="input" value="Новая таблица">
         </label>
