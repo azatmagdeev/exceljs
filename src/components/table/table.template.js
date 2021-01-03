e432f22592cbd1f2fa90146c3cf50bf09c4190c4
azatmagdeev <azatmagdeev@gmail.com>
2021-01-03 20:54:30 +0300

    table template added

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -37,7 +37,6 @@ export function createTable(rowsCount = 15,) {
       .map(toChar)
       .map(createCol)
       .join('')
-  console.log(cols)
 
   rows.push(createRow(cols, ''))
 
@@ -52,46 +51,3 @@ export function createTable(rowsCount = 15,) {
 
   return rows.join('')
 }
-
-`<div class="row">
-        <div class="row-info"></div>
-        <div class="row-data">
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">A</div>
-            <div class="column">B</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-            <div class="column">C</div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="row-info">1</div>
-        <div class="row-data">
-            <div class="cell selected" contenteditable >A1</div>
-            <div class="cell" contenteditable >B1</div>
-            <div class="cell" contenteditable >C1</div>
-            <div class="cell selected" contenteditable >A1</div>
-            <div class="cell" contenteditable >B1</div>
-            <div class="cell" contenteditable >C1</div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="row-info">2</div>
-        <div class="row-data">
-            <div class="cell" contenteditable >A2</div>
-            <div class="cell" contenteditable >B2</div>
-            <div class="cell selected" contenteditable >C2</div>
-        </div>
-    </div>`
