268e5f7f6a95486cc0dccd631beba7d4309b1a98
azatmagdeev <azatmagdeev@ya.ru>
2023-06-10 19:41:46 +0300

    скрипт для деплоя на gh-pages

--- scripts/build-and-deploy.js
+++ scripts/build-and-deploy.js
@@ -4,11 +4,13 @@ const { copySync } = require('fs-extra');
 
 
 (async () => {
+
+    const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+
     try {
 
         execSync("npm run build")
-
-        const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+        
         const lastCommitMessage = execSync('git log -1 --pretty=%B').toString().trim();
         const lastCommitId = execSync('git rev-parse HEAD').toString().trim();
 
@@ -18,15 +20,27 @@ const { copySync } = require('fs-extra');
         const targetPath = path.join(__dirname, '../');
         copySync(sourcePath, targetPath);
 
+        const hasChanges = execSync('git status --porcelain').toString().trim() !== '';
+        if (hasChanges) {
+          console.log('Есть изменения для коммита.');
+        } else {
+          console.log('Нет изменений для коммита.');
+          return;
+        }
+
         execSync('git add .');
         const message = `Add built files from branch ${currentBranch} commit ${lastCommitMessage} ${lastCommitId}`
         execSync(`git commit -m "${message}"`);
         execSync('git push origin prod');
-        execSync(`git checkout ${currentBranch}`);
+        
     
     } catch (e) {
 
         console.error(e)
-        
+
+    } finally {
+
+        execSync(`git checkout ${currentBranch}`);
+
     }
 })()
