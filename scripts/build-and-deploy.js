4660b9b75e30b89467039a6b43467cfe508d0a52
azatmagdeev <azatmagdeev@ya.ru>
2023-06-10 19:10:53 +0300

    скрипт для деплоя на gh-pages

--- scripts/build-and-deploy.js
+++ scripts/build-and-deploy.js
@@ -1,5 +1,4 @@
 const { execSync } = require('child_process');
-const fs = require('fs').promises;
 const path = require('path');
 const { copySync } = require('fs-extra');
 
@@ -8,18 +7,26 @@ const { copySync } = require('fs-extra');
     try {
 
         execSync("npm run build")
+
+        const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+        const lastCommitMessage = execSync('git log -1 --pretty=%B').toString().trim();
+        const lastCommitId = execSync('git rev-parse HEAD').toString().trim();
+
         execSync("git checkout prod");
+
         const sourcePath = path.join(__dirname, '../dist');
         const targetPath = path.join(__dirname, '../');
         copySync(sourcePath, targetPath);
+
         execSync('git add .');
-        execSync('git commit -m "Add built files"');
+        const message = `Add built files from branch ${currentBranch} commit ${lastCommitMessage} ${lastCommitId}`
+        execSync(`git commit -m "${message}"`);
         execSync('git push origin prod');
-        execSync('git checkout master');
-        
+        execSync(`git checkout ${currentBranch}`);
+    
     } catch (e) {
 
         console.error(e)
-
+        
     }
 })()
