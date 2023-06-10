d9d63c69926448b3b6a161605395e9e313909aca
azatmagdeev <azatmagdeev@ya.ru>
2023-06-10 18:15:38 +0300

    test

--- scripts/build-and-deploy.js
+++ scripts/build-and-deploy.js
@@ -1,25 +1,22 @@
 const { execSync } = require('child_process');
 const fs = require('fs').promises;
 const path = require('path');
+const { copySync } = require('fs-extra');
+
+
 (async () => {
     try {
 
         execSync("npm run build")
-        const distPath = path.join(__dirname, '../dist');
-        const files = await fs.readdir(distPath);
-
-        console.log(files)
-        const prodFiles = {};
-        for (const file of files) {
-            const filePath = path.join(distPath, file);
-            const fileContent = await fs.readFile(filePath, 'utf8');
-
-            prodFiles[file] = fileContent
-        }
-
         execSync("git checkout prod");
-
-
+        const sourcePath = join(__dirname, '../dist');
+        const targetPath = join(__dirname, '../');
+        copySync(sourcePath, targetPath);
+        execSync('git add .');
+        execSync('git commit -m "Add built files"');
+        execSync('git push origin prod');
+        execSync('git checkout master');
+        
     } catch (e) {
 
         console.error(e)
